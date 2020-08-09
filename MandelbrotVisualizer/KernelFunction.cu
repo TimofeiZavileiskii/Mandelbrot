40e962a94d76c78259d7307e12eca678f84c1b50
Timofei Zavileiskii <K036829@eltham-college.org.uk>
2020-08-10 00:07:19 +0100

    Finilized the optimization

--- MandelbrotVisualizer/KernelFunction.cu
+++ MandelbrotVisualizer/KernelFunction.cu
@@ -3,64 +3,48 @@
 #include "iostream"
 
 __global__ void feelScreenGPU(int* screen, int ScreenWidth, int ScreenHeight, double leftB, double downB, double pWidth, double pHeight, int iterations) {
+	
 	int threadNum = blockIdx.x * blockDim.x + threadIdx.x;
-	bool found = false;
-	int color = 0;
-	bool print = false;
-	if (threadNum == 529469) {
-		print = true;
-	}
 
-	if (threadNum < ScreenWidth * ScreenHeight) {
+
+	//if (threadNum < ScreenWidth * ScreenHeight) {
 		int count = 0;
 		double r1 = 0;
-		double r2 = leftB + pWidth * (double)(threadNum % ScreenWidth);
+		double r2 = leftB + pWidth * (threadNum % ScreenWidth);
 		double c1 = 0;
-		double c2 = downB + pHeight * (double)(threadNum / ScreenHeight);
-
-		
+		double c2 = downB + pHeight * (threadNum / ScreenHeight);
 
 
 		while (count < iterations)
 		{	
-			if (print & screen[threadNum] == 0) printf("Count: %d, r1: %f, c1: %f, r2: %f, c2: %f2 \n", count, r1, c1, r2, c2);
+			double r1Temp = r1;
 			r1 = r1 * r1 - c1 * c1 + r2;
-			c1 = 2 * r1 * c1 + c2;
+			c1 = 2 * r1Temp * c1 + c2;
 
 
-			if ((r1 * r1 + c1 * c1) < 4) {
-				count++;
+			if ((r1 * r1 + c1 * c1) > 4) {
+				break;
 			}
-			else{
-				screen[threadNum] = count;
-				return;
-			}
+			count++;
 		}
-		screen[threadNum] = -1;
-	}
+		screen[threadNum] = count;
+	//}
 }
 
-int* CalculateScreen(int ScreenWidth, int ScreenHeight, double leftB, double downB, double pWidth, double pHeight, int iterations, int Blocks, int Threads) {
+void CalculateScreen(int* screen, int ScreenWidth, int ScreenHeight, double leftB, double downB, double pWidth, double pHeight, int iterations, int Blocks, int Threads) {
 	
-	int* screen;
-	cudaMallocManaged(&screen, ScreenHeight * ScreenWidth * sizeof(int));
 	
-	for (int i = 0; i < ScreenWidth * ScreenHeight; i++) {
-		screen[i] = 0;
-	}
 	
-
 	feelScreenGPU <<<Blocks, Threads>>> (screen, ScreenWidth, ScreenHeight, leftB, downB, pWidth, pHeight, iterations);
 	cudaDeviceSynchronize();
-
-
-	return screen;
+	
 }
 
 void FreeMem(int* screen) {
 	cudaFree(screen);
 }
 
-void AllocateMem(int* screen, int memSize) {
+int* AllocateMem(int* screen, int memSize) {
 	cudaMallocManaged(&screen, memSize * sizeof(int));
+	return screen;
 }
--- MandelbrotVisualizer/Source.cpp
+++ MandelbrotVisualizer/Source.cpp
@@ -2,47 +2,15 @@
 #include "olcPixelGameEngine.h"
 #include "math.h"
 #include "KernelHeader.cuh"
+#include <thread>;
 
 
-struct complexNum {
-public:
-	double realPart;
-	double imagenaryPart;
-
-	complexNum(double r = 0, double i = 0) {
-		realPart = r;
-		imagenaryPart = i;
-	}
-
-	
-	complexNum operator+(complexNum& z) const {
-		return complexNum(realPart + z.realPart, imagenaryPart + z.imagenaryPart);
-	}
-
-	complexNum operator-(complexNum& z) const {
-		return complexNum(realPart - z.realPart, imagenaryPart - z.imagenaryPart);
-	}
-
-	complexNum operator*(complexNum& z) const {
-		return complexNum(realPart * z.realPart - imagenaryPart * z.imagenaryPart, imagenaryPart * z.realPart + realPart * z.imagenaryPart );
-	}
-
-	complexNum operator*(const int& z) const {
-		return complexNum(realPart * z, imagenaryPart * z);
-	}
-	
-
-	double absoluteSqr() const{
-		return realPart * realPart + imagenaryPart * imagenaryPart;
-	}
-
-};
 
 // Override base class with your custom functionality
 class Visualizer : public olc::PixelGameEngine
 {
 public:
-	const int ZoomRes = 40;
+	const int ZoomRes = 150;
 	int iterations;
 	double rightB;
 	double leftB;
@@ -55,75 +23,28 @@ public:
 	int MousePosX;
 	int MousePosY;
 	bool dragging;
-	bool scrolling;
 
 	int* screen;
 
 	Visualizer()
 	{
 		sAppName = "Mandelbrot";
-		scrolling = false;
 	}
 
 public:
-	int CheckPartOfSet(complexNum z) {
-		int count = 0;
-		complexNum var (0, 0);
-		while (count < iterations)
-		{
-			var = (var * var) + z;
 
-				if (var.absoluteSqr() < 4) {
-					count++;
-				}
-				else {
-					return count;
-					break;
-				}
-		}
-		return -1;
-	}
-
-	void feelScreen(int* screen) {
-		for (int i = 0; i < ScreenHeight() * ScreenWidth(); i++) {
-				complexNum z((leftB + (i % ScreenWidth()) * pWidth), (downB + (i/ScreenHeight()) * pHeight));
-				screen[i] = CheckPartOfSet(z);
-		}
-	}
-	
 	void drawScreen(int* screen) {
 		for (int i = 0; i < ScreenHeight() * ScreenWidth(); i++) {
-				int colour;
+				
+			int red = std::floor(255 * screen[i] / iterations);
 
-				if (screen[i] == -1) {
-					colour = 0;
-				}
-				else {
-					colour = std::floor(255 - 255 * ((double)screen[i] / double(iterations)));
-				}
+
+				int colour = std::floor(255 - 255 * ((double)screen[i] / double(iterations)));
+				
 				Draw(i % ScreenWidth(), i / ScreenHeight(), olc::Pixel(colour, colour, colour));
 		}
 	}
 	
-
-	void FeelAndDrawScreen() {
-		for (int i = 0; i < ScreenHeight(); i++) {
-			for (int ii = 0; ii < ScreenWidth(); ii++) {
-
-				complexNum z((leftB + ii * pWidth), (downB + i * pHeight));
-				int numIterations = CheckPartOfSet(z);
-				int color;
-
-				if (numIterations == -1) {
-					color = 0;
-				}
-				else {
-					color = std::floor((double)255 * ((double)1 - (double)numIterations / (double)iterations));
-				}
-				Draw(ii, i, olc::Pixel(color, color, color));
-			}
-		}
-	}
 
 	//Function callled at the beggining
 	bool OnUserCreate() override
@@ -132,17 +53,13 @@ public:
 		leftB = -2;
 		topB = 2;
 		downB = -2;
-		iterations = 128;
+		iterations = 5000;
 		dragging = false;
 
 		threads = 128;
 		blocks = (ScreenHeight() * ScreenWidth()) / threads + 1;
-		screen = new int[ScreenWidth() * ScreenHeight() * sizeof(int) * 4];
-		for (int i = 0; i < ScreenWidth() * ScreenHeight(); i++) {
-			screen[i] = i;
-		}
 
-	//	AllocateMem(screen, ScreenWidth() * ScreenHeight());
+		screen = AllocateMem(screen, ScreenWidth() * ScreenHeight());
 		
 		return true;
 	}
@@ -182,7 +99,7 @@ public:
 	}
 
 	void CalculateZoom() {
-		if (GetKey(olc::Key::Q).bHeld) {
+		if (GetKey(olc::Key::Q).bPressed) {
 			double xPos = GetMouseXInSpace(GetMouseX());
 			double yPos = GetMouseYInSpace(GetMouseY());
 			
@@ -210,10 +127,10 @@ public:
 
 	void AdjustIterations() {
 		if (GetKey(olc::Key::W).bHeld) {
-			iterations = iterations * 2;
+			iterations = iterations + 1500;
 		}
-		else if (GetKey(olc::Key::E).bHeld && iterations >= 16) {
-			iterations = iterations / 2;
+		else if (GetKey(olc::Key::E).bHeld && iterations > 1500) {
+			iterations = iterations - 1500;
 		}
 	}
 
@@ -222,53 +139,27 @@ public:
 		ss << iterations;
 		std::string str;
 		ss >> str;
-		std::string text = "Iterations are " + str;
+		std::string text = "Iterations: " + str;
 
 
 		DrawString(5, 5, text, olc::RED, 2);
 	}
 
-	void DrawCoordinates() {
-
-
-		std::stringstream ss1;
-		ss1 << GetMouseX();
-		std::string str1;
-		ss1 >> str1;
-
-		std::stringstream ss2;
-		ss2 << GetMouseY();
-		std::string str2;
-		ss2 >> str2;
-		std::string text = "X: " + str1 + " Y: " + str2;
-
-		DrawString(5, 20, text, olc::RED, 2);
-	}
 
 	//Function called every farame 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		//if (!scrolling) {
 			ChangePixelSize();
-
-
 			CalculateMouseOffset();
-
 			CalculateZoom();
 			Reset();
-
 			AdjustIterations();
 
-			feelScreen(screen);
-			//screen = CalculateScreen(ScreenWidth(), ScreenHeight(), leftB, downB, pWidth, pHeight, iterations, blocks, threads);
+
+			CalculateScreen(screen, ScreenWidth(), ScreenHeight(), leftB, downB, pWidth, pHeight, iterations, blocks, threads);
 
 			drawScreen(screen);
-			//FreeMem(screen);
-
 			DrawIterations();
-			DrawCoordinates();
-
-			scrolling = true;
 		
 		return true;
 	}
